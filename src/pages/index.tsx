9dacea73d43ed3363ee83f41f08e59c1486cf189
Josimar16 <josimarjr479@gmail.com>
2021-09-19 19:37:05 -0300

    feat: :sparkles: Criando pagina de dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import React from "react";
 import Head from "next/head";
-import { Box, Center, Divider, Icon, Flex, Text } from "@chakra-ui/react";
-import { RiLogoutBoxLine } from "react-icons/ri"
+import dynamic from 'next/dynamic';
+import { Box, Flex, SimpleGrid, Text, Grid, Icon } from "@chakra-ui/react";
+import { RiTimeLine } from "react-icons/ri";
 
-import { Profile } from "../components/Header/Profile";
-import { Logo } from "../components/Header/Logo";
+import { Header } from "../components/Header";
+import { optionsChart } from "../utils/optionsChart";
+import { BoxCount } from "../components/Dashboard/BoxCount";
+import { CopyRight } from "../components/CopyRight";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false,
+});
+
+const going = [
+    { name: 'Andamento fora do prazo', data: [10, 119, 10, 28, 45, 5, 98] },
+    { name: 'Andamento dentro do prazo', data: [120, 68, 23, 78, 18, 8, 106] },
+    { name: 'Nao iniciadas', data: [31, 10, 59, 40, 2, 116, 109] }
+];
+
+const completed = [
+    { name: 'Concluidas fora do prazo', data: [2, 60, 78, 78, 48, 20, 6] },
+    { name: 'Concluidas dentro do prazo', data: [31, 10, 59, 40, 2, 116, 109] },
+    { name: 'Canceladas', data: [16, 40, 10, 28, 45, 18, 13] },
+];
 
 export default function Home() {
     return (
@@ -11,59 +31,110 @@ export default function Home() {
             <Head>
                 <title>Home | orderflow</title>
             </Head>
-            <Flex flexDirection="column">
+            <Header />
+            <Flex
+                h="calc(100vh - 7.5rem)"
+                overflowY="auto"
+                flexDirection="column"
+
+                width="1120px"
+                p="2rem"
+                m="0 auto"
+
+                css={{
+                    '&::-webkit-scrollbar': {
+                        width: '0rem',
+                        height: '0.375rem'
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                        borderRadius: '4px'
+                    },
+                }}
+            >
                 <Flex
-                    w="100vw"
-                    h="5rem"
-                    bgColor="gray.800"
+                    justifyContent="space-between"
+                    alignItems="flex-start"
+                    mb={4}
                 >
-                    <Flex
-                        justifyContent="space-between"
-                        alignItems="center"
-
-                        w="1120px"
-                        p="0 2rem"
-                        m="0 auto"
-                    >
-                        <Logo />
-                        <Center h="2.5rem">
-                            <Icon
-                                as={RiLogoutBoxLine}
-                                h="1.5rem"
-                                w="1.5rem"
-                                color="gray.200"
+                    <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+                        <Box
+                            padding={["6", "8"]}
+                            backgroundColor="gray.800"
+                            borderRadius={8}
+                            paddingBottom="4"
+                        >
+                            <Text fontSize="lg" marginBottom="4">
+                                Solicitacoes correntes do mes {new Date().getMonth() + 1}
+                            </Text>
+                            <Chart
+                                type="area"
+                                height={200}
+                                options={{
+                                    ...optionsChart,
+                                    colors: ['#F56565', '#48BB78', '#A0AEC0'],
+                                }}
+                                series={going}
                             />
-                            <Divider orientation="vertical" mx={4} />
-                            <Profile />
-                        </Center>
-                    </Flex>
+                        </Box>
+                        <Box
+                            padding={["6", "8"]}
+                            backgroundColor="gray.800"
+                            borderRadius={8}
+                            paddingBottom="4"
+                        >
+                            <Text fontSize="lg" marginBottom="4">
+                                Solicitacoes concluidas do mes {new Date().getMonth() + 1}
+                            </Text>
+                            <Chart
+                                type="area"
+                                height={200}
+                                options={{
+                                    ...optionsChart,
+                                    colors: ['#F56565', '#48BB78', '#ED8936'],
+                                }}
+                                series={completed}
+                            />
+                        </Box>
+                    </SimpleGrid>
                 </Flex>
-                <Box
-                    h="calc(100vh - 5rem)"
-                    overflowY="auto"
-                    css={{
-                        '&::-webkit-scrollbar': {
-                            width: '0.375rem',
-                            height: '0.375rem'
-                        },
-                        '&::-webkit-scrollbar-thumb': {
-                            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-                            borderRadius: '4px'
-                        },
-                    }}
+                <Flex
+                    justifyContent="space-between"
+                    alignItems="flex-start"
+                    flexDirection="column"
                 >
-                    <Flex
-                        flex="1"
-                        flexDirection="column"
-                        justifyContent="space-between"
-                        alignItems="flex-start"
-
-                        maxWidth="1120px"
-                        p="2rem"
-                        m="0 auto"
-                    ></Flex>
-                </Box>
+                    <Text fontSize="lg" marginBottom="4">
+                        Solicitacoes no geral em andamento
+                    </Text>
+                    <Grid
+                        gap={4}
+                        templateRows="repeat(2, 1fr)"
+                        templateColumns="repeat(2, 1fr)"
+                    >
+                        <BoxCount
+                            title="Vencidas"
+                            count={20}
+                            color="red.500"
+                        />
+                        <BoxCount
+                            title="Vence hoje"
+                            count={8}
+                            color="orange.500"
+                        />
+                        <BoxCount
+                            title="Vence amanha"
+                            count={3}
+                            color="blue.500"
+                        />
+                        <BoxCount
+                            title="Vence nos proximos dias"
+                            count={45}
+                            color="green.500"
+                        />
+                    </Grid>
+                </Flex>
             </Flex>
+            <CopyRight />
         </>
     );
 }
